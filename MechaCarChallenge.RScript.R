e9a7e7b018f1f5c5bea1321b2431e1c1fdde9fb0
kmatias252 <91925639+kmatias252@users.noreply.github.com>
2022-02-06 11:53:10 -0500

    Updated Deliverable 2

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -11,3 +11,10 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 
 # Summary
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar_mpg))
+
+# Deliverable 2
+Suspension_Coils <- read.csv('/Users/katelymatias/Documents/Columbia_University/Module_15/MechaCar_Statistical_Analysis/Suspension_Coil.csv', check.names = F)
+
+total_summary<- Suspension_Coils %>% summarize(Mean_PSI=mean(PSI), Median_PSI=median(PSI),Var_PSI=var(PSI),SD_PSI=sd(PSI),.groups = 'keep')
+
+lot_summary<- Suspension_Coils %>% group_by(Manufacturing_Lot) %>% summarize(Mean_PSI=mean(PSI), Median_PSI=median(PSI),Var_PSI=var(PSI),SD_PSI=sd(PSI),.groups = 'keep')
